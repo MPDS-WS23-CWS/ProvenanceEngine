8496c26d68c5721b526b9a0615557187e7fb3fa3
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-12-11 16:22:49 +0100

    spring boot init

---
+++ ProvenanceEngine/src/test/java/com/groupGreen/ProvenanceEngine/ProvenanceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupGreen.ProvenanceEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvenanceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
