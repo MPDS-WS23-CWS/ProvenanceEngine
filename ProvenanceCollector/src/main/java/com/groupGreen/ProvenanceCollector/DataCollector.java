354485f7c21fff6254624045c21067f316383d11
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-12-17 20:28:54 +0100

    wording

--- ProvenanceCollector/src/main/java/com/groupGreen/ProvenanceCollector/DataCollector.java
+++ ProvenanceCollector/src/main/java/com/groupGreen/ProvenanceCollector/DataCollector.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProvenanceEngineApplication {
+public class DataCollector {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProvenanceEngineApplication.class, args);
+		SpringApplication.run(DataCollector.class, args);
 	}
 
 }
