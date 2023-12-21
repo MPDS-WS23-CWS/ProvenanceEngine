482f622e06b551b89126cec616fbc25a08a0fd6e
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-12-21 16:05:51 +0100

    improved fetching mechanism for queries

--- ProvenanceCollector /src/main/java/com/groupGreen/ProvenanceCollector/MetricsClient.java
+++ ProvenanceCollector /src/main/java/com/groupGreen/ProvenanceCollector/MetricsClient.java
@@ -2,33 +2,38 @@ package com.groupGreen.ProvenanceCollector;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class MetricsClient {
 
     private final WebClient webClient;
 
-    public MetricsClient() {
-        this.webClient = WebClient.builder()
-                .baseUrl("http://localhost:8080")
-                .build();
-    }
+    @Value("${prometheus.server.url}")
+    private String prometheusServerUrl;
 
-    // test logic to fetch metrics from running prom-server
+    @Value("${prometheus.queries}")
+    private String [] queries;
+
+    public MetricsClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
 
     public void fetchMetrics() {
-        String promUrl = "http://localhost:8080/api/v1/query";
-        String query = "up";
+        List<String> metrics = Arrays.asList(queries);
 
-        String url = promUrl + "?query=" + query;
-        String response = webClient.get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        for (String query : metrics) {
+            String url = prometheusServerUrl + "?query=" + query;
+            String response = webClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
 
         System.out.println("Response from Prometheus-Server: " + response);
+        }
     }
-
 }
