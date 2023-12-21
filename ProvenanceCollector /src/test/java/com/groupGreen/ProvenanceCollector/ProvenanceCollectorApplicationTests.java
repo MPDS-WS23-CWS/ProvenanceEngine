86963a86bbbb94226131280633de0a3a98a2afa7
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-12-21 11:27:26 +0100

    added simple client to query prometheus

---
+++ ProvenanceCollector /src/test/java/com/groupGreen/ProvenanceCollector/ProvenanceCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupGreen.ProvenanceCollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvenanceCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
