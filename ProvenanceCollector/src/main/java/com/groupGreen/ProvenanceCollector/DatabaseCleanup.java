ea1530af0b8cb9b861b395c0d6d4e3c6bfb39a48
Leon Schäffer <leonschaeffer@me.com>
2024-02-25 11:15:30 +0100

    Remove space from project name 'ProvenanceCollector '

---
+++ ProvenanceCollector/src/main/java/com/groupGreen/ProvenanceCollector/DatabaseCleanup.java
@@ -0,0 +1,23 @@
+package com.groupGreen.ProvenanceCollector;
+
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
+import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+//@Component
+//public class DatabaseCleanup {
+//
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    public DatabaseCleanup(DataSource datasource) {
+//        this.jdbcTemplate = new JdbcTemplate(datasource);
+//    }
+//
+//    @PreDestroy
+//    public void clearDatabase() {
+//        jdbcTemplate.execute("DELETE FROM tasks");
+//        jdbcTemplate.execute("DELETE FROM workflows");
+//        jdbcTemplate.execute("DELETE FROM resources");
+//    }
+//}
---
+++ src/test/java/com/cws/provenance_engine/ProvenanceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cws.provenance_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvenanceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
